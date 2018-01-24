f0be488fe2e552c02fddbd59409d9a71ad1ec671
2YSP <ship@ubox.cn>
2018-01-24 16:58:30 +0800

    netty 切换IO方式对比

---
+++ src/main/java/cn/sp/NettyNioServer.java
@@ -0,0 +1,55 @@
+package cn.sp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+/**
+ * @Author: 2YSP
+ * @Description: 非阻塞版本
+ * @Date: Created in 2018/1/24
+ */
+public class NettyNioServer {
+
+    /**
+     * send Hi! to client
+     * @param port
+     * @throws Exception
+     */
+    public void server(int port)throws Exception{
+        final ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress("127.0.0.1",port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    ctx.writeAndFlush(buf.duplicate())
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                }
+                            });
+                        }
+                    });
+
+            ChannelFuture future = b.bind().sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+}
---
+++ src/main/java/cn/sp/NettyOioServer.java
@@ -0,0 +1,55 @@
+package cn.sp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+/**
+ * @Author: 2YSP
+ * @Description: 使用netty的阻塞网络处理
+ * @Date: Created in 2018/1/24
+ */
+public class NettyOioServer {
+
+    /**
+     * send Hi! to client
+     * @param port
+     * @throws Exception
+     */
+    public void server(int port)throws Exception{
+        final ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+        EventLoopGroup group = new OioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(OioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress("127.0.0.1",port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    ctx.writeAndFlush(buf.duplicate())
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                }
+                            });
+                        }
+                    });
+
+            ChannelFuture future = b.bind().sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+}
